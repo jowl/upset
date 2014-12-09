3e3f21f351753463453a334eb516147ca055105f
Joel Segerlind <joel@burtcorp.com>
2014-12-09 15:26:38 +0100

    Clean up Dsl spec

--- spec/upset/definition/dsl_spec.rb
+++ spec/upset/definition/dsl_spec.rb
@@ -5,24 +5,18 @@ require 'spec_helper'
 module Upset
   module Definition
     describe Dsl do
-      let :validator do
-        Class.new.include(described_class).class_exec do
-          schema do
-            required_property('alpha')
-            optional_property('beta').is kind(String)
-            optional_property('gamma').is matching(/[A-Z]+/)
-            optional_property('delta').are all kind(Integer)
-            optional_property('epsilon').is either kind(NilClass), kind(String)
-            optional_property('zeta').is both matching(/^A/), matching(/Z$/)
-            optional_property('eta') do
-              required_property('theta')
-            end
-          end
-        end.new
+      def schema(&block)
+        Class.new.include(described_class).class_exec { schema(&block) }.new
       end
 
       describe '#schema' do
         describe '#required_property' do
+          let :validator do
+            schema do
+              required_property 'alpha'
+            end
+          end
+
           it 'adds a required property' do
             expect(validator.validate('alpha' => nil)).to be_valid
             expect(validator.validate({})).not_to be_valid
@@ -30,56 +24,99 @@ module Upset
         end
 
         describe '#optional_property' do
-          let :properties do
-            { 'alpha' => nil }
+          let :validator do
+            schema do
+              optional_property('alpha')
+            end
           end
 
           it 'adds an optional property' do
-            expect(validator.validate(properties)).to be_valid
-            expect(validator.validate(properties.merge('beta' => 'B'))).to be_valid
+            expect(validator.validate({})).to be_valid
+            expect(validator.validate('alpha' => nil)).to be_valid
           end
         end
 
-        context 'when creating constraints' do
-          let :properties do
-            { 'alpha' => nil }
+        context 'with constraints' do
+          describe '#kind' do
+            let :validator do
+              schema do
+                required_property('alpha').is kind(String)
+              end
+            end
+
+            it 'creates KindConstraint' do
+              expect(validator.validate('alpha' => 'A')).to be_valid
+              expect(validator.validate('alpha' => nil)).not_to be_valid
+            end
           end
 
-          it 'can create KindConstraint' do
-            expect(validator.validate(properties.merge('beta' => 'B'))).to be_valid
-            expect(validator.validate(properties.merge('beta' => nil))).not_to be_valid
+          describe '#matching' do
+            let :validator do
+              schema do
+                required_property('alpha').is matching(/[A-Z]+/)
+              end
+            end
+
+            it 'creates RegexpConstraint' do
+              expect(validator.validate('alpha' => 'ABC')).to be_valid
+              expect(validator.validate('alpha' => 'abc')).not_to be_valid
+            end
           end
 
-          it 'can create RegexpConstraint' do
-            expect(validator.validate(properties.merge('gamma' => 'ABC'))).to be_valid
-            expect(validator.validate(properties.merge('gamma' => 'abc'))).not_to be_valid
+          describe '#all' do
+            let :validator do
+              schema do
+                required_property('alpha').are all kind(Integer)
+              end
+            end
+
+            it 'creates MemberConstraint' do
+              expect(validator.validate('alpha' => [1, 2, 3])).to be_valid
+              expect(validator.validate('alpha' => [1, nil, 3])).not_to be_valid
+            end
           end
 
-          it 'can create MemberConstraint' do
-            expect(validator.validate(properties.merge('delta' => [1, 2, 3]))).to be_valid
-            expect(validator.validate(properties.merge('delta' => [1, nil, 3]))).not_to be_valid
+          describe '#either' do
+            let :validator do
+              schema do
+                required_property('alpha').is either kind(String), kind(NilClass)
+              end
+            end
+
+            it 'creates DisjunctiveConstraint' do
+              expect(validator.validate('alpha' => nil)).to be_valid
+              expect(validator.validate('alpha' => 'A')).to be_valid
+              expect(validator.validate('alpha' => 1)).not_to be_valid
+            end
           end
 
-          it 'can create DisjunctiveConstraint' do
-            expect(validator.validate(properties.merge('epsilon' => nil))).to be_valid
-            expect(validator.validate(properties.merge('epsilon' => 'E'))).to be_valid
-            expect(validator.validate(properties.merge('epsilon' => 1))).not_to be_valid
-          end
+          describe '#either' do
+            let :validator do
+              schema do
+                required_property('alpha').is both matching(/^A/), matching(/Z$/)
+              end
+            end
 
-          it 'can create ConjunctiveConstraint' do
-            expect(validator.validate(properties.merge('zeta' => 'A-Z'))).to be_valid
-            expect(validator.validate(properties.merge('zeta' => 'B-Y'))).not_to be_valid
+            it 'creates ConjunctiveConstraint' do
+              expect(validator.validate('alpha' => 'A-Z')).to be_valid
+              expect(validator.validate('alpha' => 'B-Z')).not_to be_valid
+              expect(validator.validate('alpha' => 'A-Y')).not_to be_valid
+            end
           end
         end
 
         context 'when nesting schemas' do
-          let :properties do
-            { 'alpha' => nil }
+          let :validator do
+            schema do
+              optional_property('alpha') do
+                required_property('beta')
+              end
+            end
           end
 
           it 'adds definitions for nested properties' do
-            expect(validator.validate(properties.merge('eta' => { }))).not_to be_valid
-            expect(validator.validate(properties.merge('eta' => { 'theta' => nil }))).to be_valid
+            expect(validator.validate('alpha' => { })).not_to be_valid
+            expect(validator.validate('alpha' => { 'beta' => nil })).to be_valid
           end
         end
       end
