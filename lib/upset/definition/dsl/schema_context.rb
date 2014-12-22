ad076eafb3aed85856df7b0c009fd912bc8c7f30
Joel Segerlind <joel@burtcorp.com>
2014-12-23 00:56:22 +0100

    Another go at the definition DSL

--- lib/upset/definition/dsl/schema_context.rb
+++ lib/upset/definition/dsl/schema_context.rb
@@ -8,50 +8,75 @@ module Upset
       class SchemaContext
         include Constraints
 
-        attr_reader :property_definitions
+        attr_reader :property_contexts
         def initialize(&schema_definition)
-          @property_definitions = {}
+          @property_contexts = {}
           instance_exec(&schema_definition) if schema_definition
         end
 
-        def required_property(key, constraint=nil, &block)
-          @property_definitions[key] = property_definition(false, constraint, block, key)
+        def required(key, &block)
+          @property_contexts[key] = property_context(false, block)
         end
 
-        def optional_property(key, constraint=nil, &block)
-          @property_definitions[key] = property_definition(true, constraint, block, key)
+        def optional(key, &block)
+          @property_contexts[key] = property_context(true, block)
         end
 
-        def default(constraint=nil, &block)
-          @property_definitions.default = property_definition(true, constraint, block)
+        def residual(&block)
+          @property_contexts.default = property_context(true, block)
         end
 
         def build
-          Schema.new(@property_definitions)
+          property_definitions = Hash[@property_contexts.map { |k, v| [k, v.build] }]
+          if @property_contexts.default
+            property_definitions.default = @property_contexts.default.build
+          end
+          Schema.new(property_definitions)
         end
 
         private
 
         def initialize_dup(other)
           super
-          @property_definitions = other.property_definitions.dup
+          @property_contexts = other.property_contexts.dup
         end
 
-        def property_definition(optional, constraint, schema_definition, key=nil)
-          if schema_definition && constraint
-            key = key ? "Property #{key.inspect}" : 'Default property'
-            raise SchemaError, "#{key} has both constraint and schema"
-          elsif schema_definition
-            schema = SchemaContext.new(&schema_definition).build
-            SchemaProperty.new(schema, optional)
+        def property_context(optional, schema_definition)
+          if schema_definition
+            SchemaPropertyContext.new(optional, schema_definition)
           else
-            constraint ||= ValidConstraint.new
-            ValueProperty.new(constraint, optional)
+            ValuePropertyContext.new(optional)
+          end
+        end
+
+        class ValuePropertyContext
+          def initialize(optional)
+            @optional = optional
+            @constraint = ValidConstraint.new
+          end
+
+          def is(constraint)
+            @constraint = constraint
+          end
+          alias :are :is
+
+          def build
+            ValueProperty.new(@constraint, @optional)
+          end
+        end
+
+        class SchemaPropertyContext
+          def initialize(optional, schema_definition)
+            @optional = optional
+            @schema_definition = schema_definition
+          end
+
+          def build
+            schema = SchemaContext.new(&@schema_definition).build
+            SchemaProperty.new(schema, @optional)
           end
         end
       end
-
-      SchemaError = Class.new(UpsetError)
     end
   end
 end
