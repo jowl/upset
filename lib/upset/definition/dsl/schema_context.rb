89642338231081b913b11f63557de4457d5e8c9d
Joel Segerlind <joel@burtcorp.com>
2014-12-23 01:02:09 +0100

    Refactor SchemaContext

--- lib/upset/definition/dsl/schema_context.rb
+++ lib/upset/definition/dsl/schema_context.rb
@@ -15,19 +15,19 @@ module Upset
         end
 
         def required(key, &block)
-          @property_contexts[key] = property_context(false, block)
+          @property_contexts[key] = PropertyContext.new(false, block)
         end
 
         def optional(key, &block)
-          @property_contexts[key] = property_context(true, block)
+          @property_contexts[key] = PropertyContext.new(true, block)
         end
 
         def residual(&block)
-          @property_contexts.default = property_context(true, block)
+          @property_contexts.default = PropertyContext.new(true, block)
         end
 
         def build
-          property_definitions = Hash[@property_contexts.map { |k, v| [k, v.build] }]
+          property_definitions = Hash[@property_contexts.map { |key, property_context| [key, property_context.build] }]
           if @property_contexts.default
             property_definitions.default = @property_contexts.default.build
           end
@@ -41,18 +41,10 @@ module Upset
           @property_contexts = other.property_contexts.dup
         end
 
-        def property_context(optional, schema_definition)
-          if schema_definition
-            SchemaPropertyContext.new(optional, schema_definition)
-          else
-            ValuePropertyContext.new(optional)
-          end
-        end
-
-        class ValuePropertyContext
-          def initialize(optional)
+        class PropertyContext
+          def initialize(optional, schema_definition)
             @optional = optional
-            @constraint = ValidConstraint.new
+            @schema_definition = schema_definition
           end
 
           def is(constraint)
@@ -61,19 +53,12 @@ module Upset
           alias :are :is
 
           def build
-            ValueProperty.new(@constraint, @optional)
-          end
-        end
-
-        class SchemaPropertyContext
-          def initialize(optional, schema_definition)
-            @optional = optional
-            @schema_definition = schema_definition
-          end
-
-          def build
-            schema = SchemaContext.new(&@schema_definition).build
-            SchemaProperty.new(schema, @optional)
+            if @schema_definition
+              schema = SchemaContext.new(&@schema_definition).build
+              SchemaProperty.new(schema, @optional)
+            else
+              ValueProperty.new(@constraint || ValidConstraint.new, @optional)
+            end
           end
         end
       end
